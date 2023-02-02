8ccbce711e1abbe9a53c62e1087fffccd4044310
sebsa-mirror <nobody@sebsa.moe>
2023-02-02 23:33:43 +0800

    chore: 🚀 build project for production use

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,1 +1,9 @@
-export * from "./lib/ServerProtocolBuffers";
+import * as ServerProtocolBuffers from "./lib/ServerProtocolBuffers";
+import * as GatewayProtocolBuffers from "./lib/GatewayProtocolBuffers";
+declare const _default: {
+    Protobuf: {
+        Server: typeof ServerProtocolBuffers;
+        Gateway: typeof GatewayProtocolBuffers;
+    };
+};
+export default _default;
