4ce118a8030415b3123ffc73d49c88f25ffd7a6c
sebsa-mirror <nobody@sebsa.moe>
2023-02-02 23:22:03 +0800

    chore(GatewayProtocolBuffers): 🚀 generate from proto

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from "./lib/ServerProtocolBuffers";
+import * as ServerProtocolBuffers from "./lib/ServerProtocolBuffers";
+import * as GatewayProtocolBuffers from "./lib/GatewayProtocolBuffers";
+
+export default {
+	Protobuf: {
+		Server: ServerProtocolBuffers
+	}
+};
